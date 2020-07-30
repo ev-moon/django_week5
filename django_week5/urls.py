53d512c5c8233cf4e66773f193b3d3e961d1a05f
ev-moon <xenos96111@gmail.com>
2020-07-30 21:06:17 +0900

    deploy settings

--- django_week5/urls.py
+++ django_week5/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name='home'),
+    path('accounts/', include('allauth.urls')),
 ]
